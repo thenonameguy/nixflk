8a62631310914d85704aca16f0c80ac0564cb7a0
Timothy DeHerrera <tim.deh@pm.me>
2020-07-16 15:11:08 -0600

    nrd#vpn: make resolv.conf immutable

--- users/nrd/vpn.nix
+++ users/nrd/vpn.nix
@@ -10,26 +10,35 @@ in {
   imports = [ ../../profiles/misc/stubby.nix ];
 
   services.openvpn.servers = {
-    "US-CO_6" = {
+    "US-CO_6" = with pkgs; {
       config = ''
         ${config}
         auth-user-pass ${pass}
       '';
       up = ''
+        ${e2fsprogs}/bin/chattr -i /etc/resolv.conf
         mv /etc/resolv.conf /etc/resolv.conf.old
         echo nameserver $nameserver > /etc/resolv.conf
         echo options edns0 >> /etc/resolv.conf
+        ${e2fsprogs}/bin/chattr +i /etc/resolv.conf
       '';
-      down = "mv /etc/resolv.conf.old /etc/resolv.conf";
+      down = ''
+        ${e2fsprogs}/bin/chattr -i /etc/resolv.conf
+        mv /etc/resolv.conf.old /etc/resolv.conf
+        ${e2fsprogs}/bin/chattr +i /etc/resolv.conf
+      '';
     };
   };
 
   # needed to prevent dnsleaks on nixos-rebuilds
   system.activationScripts = {
     vpn = {
-      text = ''
+      text = with pkgs; ''
         if grep 127.0.0.1 /etc/resolv.conf > /dev/null; then
-          ${pkgs.systemd}/bin/systemctl restart openvpn-US-CO_6.service
+          ${e2fsprogs}/bin/chattr -i /etc/resolv.conf
+
+          ${systemd}/bin/systemctl restart openvpn-US-CO_6.service && \
+            ${e2fsprogs}/bin/chattr +i /etc/resolv.conf
         fi
       '';
       deps = [ ];
