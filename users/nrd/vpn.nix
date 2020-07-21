667ab1450f363657766d13a954748dda00183091
Timothy DeHerrera <tim.deh@pm.me>
2020-07-21 11:36:35 -0600

    vpn: create protonvpn systemd service

--- users/nrd/vpn.nix
+++ users/nrd/vpn.nix
@@ -2,9 +2,23 @@
 
   environment.systemPackages = with unstablePkgs; [ protonvpn-cli-ng ];
 
-  # needed to prevent dnsleaks on nixos-rebuilds
-  system.activationScripts.vpn = with unstablePkgs; ''
-    PATH=${which}/bin:${iproute}/bin:${procps}/bin:$PATH
-    ${protonvpn-cli-ng}/bin/protonvpn c -f
-  '';
+  systemd.services.protonvpn = with unstablePkgs; {
+    enable = true;
+    after = [ "network-online.target" ];
+    description = "Auto-Connect to Fastest ProtonVPN Server";
+    wantedBy = [ "multi-user.target" ];
+
+    path = [ which iproute procps ];
+
+    environment = {
+      PVPN_WAIT = "300";
+      PVPN_DEBUG = "1";
+    };
+
+    serviceConfig = {
+      Type = "forking";
+      ExecStart = "${protonvpn-cli-ng}/bin/protonvpn c -f";
+      ExecStop = "${protonvpn-cli-ng}/bin/protonvpn d";
+    };
+  };
 }
