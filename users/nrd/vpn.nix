7d6d58e708b15dc5c51d844bef3f82be21f69f5d
Timothy DeHerrera <tim.deh@pm.me>
2020-07-20 18:39:08 -0600

    vpn: simplify vpn by using protonvpn-cli

--- users/nrd/vpn.nix
+++ users/nrd/vpn.nix
@@ -1,47 +1,10 @@
-{ lib, pkgs, ... }:
-let
-  inherit (lib) fileContents;
-  inherit (pkgs) writeText;
+{ unstablePkgs, ... }: {
 
-  config = fileContents ../../secrets/us-co-06.ovpn;
-  vpn = fileContents ../../secrets/vpn;
-  pass = writeText "vpn" vpn;
-in {
-  imports = [ ../../profiles/misc/stubby.nix ];
-
-  services.openvpn.servers = {
-    "US-CO_6" = with pkgs; {
-      config = ''
-        ${config}
-        auth-user-pass ${pass}
-      '';
-      up = ''
-        ${e2fsprogs}/bin/chattr -i /etc/resolv.conf
-        mv /etc/resolv.conf /etc/resolv.conf.old
-        echo nameserver $nameserver > /etc/resolv.conf
-        echo options edns0 >> /etc/resolv.conf
-        ${e2fsprogs}/bin/chattr +i /etc/resolv.conf
-      '';
-      down = ''
-        ${e2fsprogs}/bin/chattr -i /etc/resolv.conf
-        mv /etc/resolv.conf.old /etc/resolv.conf
-        ${e2fsprogs}/bin/chattr +i /etc/resolv.conf
-      '';
-    };
-  };
+  environment.systemPackages = with unstablePkgs; [ protonvpn-cli-ng ];
 
   # needed to prevent dnsleaks on nixos-rebuilds
-  system.activationScripts = {
-    vpn = {
-      text = with pkgs; ''
-        if grep 127.0.0.1 /etc/resolv.conf > /dev/null; then
-          ${e2fsprogs}/bin/chattr -i /etc/resolv.conf
-
-          ${systemd}/bin/systemctl restart openvpn-US-CO_6.service && \
-            ${e2fsprogs}/bin/chattr +i /etc/resolv.conf
-        fi
-      '';
-      deps = [ ];
-    };
-  };
+  system.activationScripts.vpn = with unstablePkgs; ''
+    PATH=${which}/bin:${iproute}/bin:${procps}/bin:$PATH
+    ${protonvpn-cli-ng}/bin/protonvpn c -f
+  '';
 }
