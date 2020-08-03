c6093f085d12e35f4cf018cac221b0d4980db211
Timothy DeHerrera <tim.deh@pm.me>
2020-08-02 21:42:45 -0600

    remove everything

--- modules/list.nix
+++
@@ -1,7 +0,0 @@
-[
-  ./services/torrent/qbittorrent.nix
-  ./security/mitigations.nix
-  ./services/hardware/wii-u-gc-adapter.nix
-  ./services/games/gamemode.nix
-  ./services/x11/window-managers/steam.nix
-]
