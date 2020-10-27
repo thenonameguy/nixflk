73502a40bfce9d8afbbcaf02c8a8a7f8545edc67
Krisztian Szabo <thenonameguy24@gmail.com>
2020-10-27 19:03:23 +0100

    Set up cachix

---
+++ cachix/thenonameguy.nix
@@ -0,0 +1,12 @@
+
+{
+  nix = {
+    binaryCaches = [
+      "https://thenonameguy.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "thenonameguy.cachix.org-1:0XO1PorsXZ0m39Qy+zGml4fi1jjdCBN8FcB0znlyBQc="
+    ];
+  };
+}
+    
